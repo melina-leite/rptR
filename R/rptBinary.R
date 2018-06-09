a40479eb7e98c54c1db83713e891e23493139434
melina de souza leite <melina.leite@ib.usp.br>
2018-06-09 14:47:29 -0300

    modificando binary para incluir otimizador no glmer
    modifiquei a função para permitir incluir os parâmetros de otimização do modelo para permitir convergência.

--- R/rptBinary.R
+++ R/rptBinary.R
@@ -97,7 +97,8 @@
 
 rptBinary <- function(formula, grname, data, link = c("logit", "probit"), CI = 0.95, nboot = 1000, 
         npermut = 0, parallel = FALSE, ncores = NULL, ratio = TRUE, adjusted = TRUE, expect="meanobs",
-        rptObj = NULL, update = FALSE) {
+        rptObj = NULL, update = FALSE,
+        nAGQ = 1, control = glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 500000))) {
         
         # missing values
         no_NA_vals <- stats::complete.cases(data[all.vars(formula)])
@@ -121,7 +122,8 @@ rptBinary <- function(formula, grname, data, link = c("logit", "probit"), CI = 0
         if (!(link %in% c("logit", "probit"))) stop("Link function has to be 'logit' or 'probit'")
         
         # note: no Overdispersion for binary data. 
-        mod <- lme4::glmer(formula, data = data, family = stats::binomial(link = link))
+        mod <- lme4::glmer(formula, data = data, family = stats::binomial(link = link),
+                           nAGQ = nAGQ, control = control)
         
         # check for random slopes
         VarComps <- lme4::VarCorr(mod)
@@ -173,7 +175,7 @@ rptBinary <- function(formula, grname, data, link = c("logit", "probit"), CI = 0
         # point estimates of R
         R_pe <- function(formula, data, grname) {
                 
-                mod <- lme4::glmer(formula = formula, data = data, family = stats::binomial(link = link))
+                mod <- lme4::glmer(formula = formula, data = data, family = stats::binomial(link = link), nAGQ = nAGQ, control = control)
                 
                 # mod <- lme4::glmer(formula = formula, data = data, family = stats::binomial(link = link))
                 # random effect variance data.frame
